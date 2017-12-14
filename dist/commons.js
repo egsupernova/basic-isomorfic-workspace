8eb6c96f16405df19bb5fcd9f0f161d976cda043
sexposito <sergi123455@gmail.com>
2017-12-14 10:51:50 -0600

    correction about server

--- dist/commons.js
+++ dist/commons.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6f5bb9d00e35017f082b"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "a31e754871927f6e53af"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
